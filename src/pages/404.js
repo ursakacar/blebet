697812316ad28fbc503b8838ee1c09f0e09ae812
Ursa Kacar <kacar.ursa@gmail.com>
2020-07-24 17:01:12 +0200

    updates 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,11 +7,10 @@ export default function NotFound() {
     return (
         <Layout page="404" bgColor="#9d3d85">
             <div className={notFoundStyles.notFound__container}>
-                <Link to="/">
-                    <h1 className={notFoundStyles.notFound__message}>Stran ne obstaja.</h1> <h1 className={notFoundStyles.notFound__message}>Mogoče nikoli ni obstajala, ali pa samo ne obstaja več. 
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+                    <h1 className={notFoundStyles.notFound__message}>Stran ne obstaja.</h1> <h1 className={notFoundStyles.notFound__message}>Mogoče nikoli ni obstajala, ali pa samo ne obstaja več.
                         </h1><h1 className={notFoundStyles.notFound__message}>Mogoče nekoč bo obstajala? Mogoče nikoli. Verjetno nikoli.</h1>
-                </Link>
-            </div>
+            </a></div>
         </Layout>
     )
 }
