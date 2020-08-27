d3afbd3c2c64aafe91f5f0e9fd51a70d50171855
Ursa Kacar <kacar.ursa@gmail.com>
2020-08-27 17:07:48 +0200

    subpages css cleanup

--- src/pages/info.js
+++ src/pages/info.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../components/Layout"
 import infoStyles from "../styles/pages/subpage.module.scss"
 import useSiteMetaData from "../static_queries/useSiteMetadata"
-import Subscribe from '../components/Subscribe'
 
 export default function Info() {
   const { infoData } = useSiteMetaData()
