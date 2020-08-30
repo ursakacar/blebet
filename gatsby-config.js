6b18649512232aca8c1e12d8ee03e64bba80ce11
Ursa Kacar <kacar.ursa@gmail.com>
2020-08-30 20:54:19 +0200

    styles 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,15 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL =  config.site_url,
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   //this makes the site config available to forestry cms
   siteMetadata: {
@@ -24,6 +33,27 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     `gatsby-plugin-sitemap`,
     "gatsby-transformer-yaml",
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
