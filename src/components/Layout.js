54ef4dafb122f5173ae99a86ca11e5c7f046a212
Ursa Kacar <kacar.ursa@gmail.com>
2020-09-04 14:02:28 +0200

    adds keywords and author

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import useSiteMetadata from "../static_queries/useSiteMetadata"
 import layoutStyles from "../styles/components/layout.module.scss"
 
 export default function Layout(props) {
-  const { title, description } = useSiteMetadata()
+  const { title, description, author, keywords } = useSiteMetadata()
   return (
     <section
       className={`${layoutStyles.layout} ${(props.page === "info" ||
@@ -20,6 +20,8 @@ export default function Layout(props) {
         <html lang="sl" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta name="author" content={author} />
+        <meta name="keywords" content={keywords} />
       </Helmet>
       <Header page={props.page} title={title} />
       <div className={layoutStyles.content}>{props.children}</div>
