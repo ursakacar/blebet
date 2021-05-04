ebfea4a2e5ace957bdfc1e199db6249d30d42b3b
Ursa Kacar <kacar.ursa@gmail.com>
2021-05-04 09:03:50 +0200

    adding preview image try 2

--- src/pages/subscription.js
+++ src/pages/subscription.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Helmet from "react-helmet"
 import subscriptionStyles from "../styles/pages/subpage.module.scss"
 import useSiteMetaData from "../static_queries/useSiteMetadata"
 import Subscribe from '../components/Subscribe'
 import subscribeImage from "../assets/blebet-subscribe2.jpg"
-
 
 export default function Subscription() {
   const { subscriptionData } = useSiteMetaData()
@@ -16,11 +16,12 @@ export default function Subscription() {
         </h2>
         <Subscribe />
       </section>
-      <img
-        src={subscribeImage}
-        alt="blebet-subscribe-image"
-        className={subscriptionStyles.thumb_image}
-      />
+      <Helmet>
+        <title>Prijava na nove objave</title>
+        <meta property="og:image" content={subscribeImage} />
+        <meta property="og:description" content="Zaupajte mi svoj elektronski naslov in postanite prvi obveščenec, ko se na Blebetu manifestira frišna objava." />
+        <meta name="description" content="Zaupajte mi svoj elektronski naslov in postanite prvi obveščenec, ko se na Blebetu manifestira frišna objava." />
+      </Helmet>
     </Layout>
   )
 }
